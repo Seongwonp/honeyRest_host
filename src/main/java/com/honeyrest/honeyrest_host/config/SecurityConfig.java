7868a946e1c676be71f440d56b204a21f12418de
seongwonp <swp2013@naver.com>
2025-08-12 16:40:49 +0900

    commit

--- src/main/java/com/honeyrest/honeyrest_host/config/SecurityConfig.java
+++ src/main/java/com/honeyrest/honeyrest_host/config/SecurityConfig.java
@@ -1,4 +1,23 @@
 package com.honeyrest.honeyrest_host.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@Configuration
+@EnableWebSecurity
 public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(auth -> auth
+                        .anyRequest().permitAll() // 모든 요청 허용
+                )
+                .csrf(csrf -> csrf.disable()); // 개발 중엔 CSRF 비활성화
+
+        return http.build();
+    }
 }
