c7a4d521ca3c017c716a0448dc31606e9533bcd7
seongwonp <swp2013@naver.com>
2025-08-12 16:40:15 +0900

    commit

--- src/main/java/com/honeyrest/honeyrest_host/controller/dashboard/DashboardController.java
+++ src/main/java/com/honeyrest/honeyrest_host/controller/dashboard/DashboardController.java
@@ -1,4 +1,17 @@
 package com.honeyrest.honeyrest_host.controller.dashboard;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
 public class DashboardController {
+
+    @GetMapping("/dashboard")
+    public String dashboard(Model model) {
+
+        return "dashboard/dashboard";
+    }
 }
