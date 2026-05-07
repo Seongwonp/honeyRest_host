fe39351f098e24c4184c6e85cfdc3cae9296eb2e
parkseong-won <swp2013@naver.com>
2026-05-07 09:52:04 +0900

    커밋

---
+++ src/main/resources/application_security.properties.ex
@@ -0,0 +1,37 @@
+# Nurigo SMS API credentials
+com.tjfgusdh.nurigo.apiKey=YOUR_NURIGO_API_KEY
+com.tjfgusdh.nurigo.apiSecretKey=YOUR_NURIGO_API_SECRET_KEY
+com.tjfgusdh.nurigo.domain=https://api.solapi.com
+com.tjfgusdh.nurigo.from=YOUR_NURIGO_FROM_NUMBER
+
+# Toss Payments widget credentials
+com.tjfgusdh.toss.widgetClientKey=YOUR_TOSS_WIDGET_CLIENT_KEY
+com.tjfgusdh.toss.widgetSecretKey=YOUR_TOSS_WIDGET_SECRET_KEY
+
+# Database password
+spring.datasource.password=YOUR_DATABASE_PASSWORD
+
+# JWT secret key
+jwt.secret=YOUR_JWT_SECRET_KEY
+
+# OpenWeather API
+weather.api.key=YOUR_OPENWEATHER_API_KEY
+
+# File storage (S3)
+file.storage.type=s3
+file.s3.accessKey=YOUR_S3_ACCESS_KEY
+file.s3.secretKey=YOUR_S3_SECRET_KEY
+file.s3.bucketName=YOUR_S3_BUCKET_NAME
+file.s3.endpoint=https://s3.filebase.com
+
+# Google OAuth credentials
+google.client-id=YOUR_GOOGLE_CLIENT_ID
+google.client-secret=YOUR_GOOGLE_CLIENT_SECRET
+
+# Kakao OAuth credentials
+kakao.client-id=YOUR_KAKAO_CLIENT_ID
+kakao.client-secret=YOUR_KAKAO_CLIENT_SECRET
+
+# Gmail credentials
+gmail.username=YOUR_GMAIL_USERNAME
+gmail.access-token=YOUR_GMAIL_ACCESS_TOKEN
